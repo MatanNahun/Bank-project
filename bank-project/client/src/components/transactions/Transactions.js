d49d842ff9e7604caa5c49f13979414683cfed6a
MatanNahum <matan.nahum95@gmail.com>
2022-11-18 13:45:55 +0200

    my transactions rendered done

--- bank-project/client/src/components/transactions/Transactions.js
+++ bank-project/client/src/components/transactions/Transactions.js
@@ -1,3 +1,30 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
 export default function Transactions() {
-  return <div className="transactions-container">all transactions</div>;
+  const [transactions, setTransactions] = useState([]);
+
+  useEffect(() => {
+    const getAllTransactions = async () => {
+      const transactions = await axios.get(
+        "http://localhost:8000/transactions"
+      );
+      setTransactions(transactions.data);
+    };
+    getAllTransactions();
+  }, []);
+
+  return (
+    <div className="transactions-container">
+      all transactions
+      <div>
+        {" "}
+        {transactions.map((transaction) => (
+          <div>
+            {transaction.name} | {transaction.category} | {transaction.amount}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
